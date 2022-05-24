28d43b4b2a6fee92a4b45b60314f83653efed923
Nate R <natemreinhardt@gmail.com>
2022-05-23 21:02:28 -0500

    linting

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import TitleBar from './components/TitleBar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
+  <TitleBar />
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/CECA_Logo_Submit.png"
+      width="125"
+      height="125"
+    />
+    Hello World
+  </header>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
