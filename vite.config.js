9c7ceaad6e3d77bf46e15af906e38a030df0b6c3
Nate R <natemreinhardt@gmail.com>
2022-06-02 13:26:21 -0500

    upgrade vite again

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { fileURLToPath, URL } from 'url';
-import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [{ src: './assets/**/*', dest: 'public/' }],
-    }),
-  ],
-
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
