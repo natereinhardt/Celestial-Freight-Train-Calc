31f6b9293477003d8eb7565ceb4ec79fcfde4f5f
Nate R <natemreinhardt@gmail.com>
2022-06-02 13:20:40 -0500

    copy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { fileURLToPath, URL } from 'url';
-
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [{ src: './assets/**/*', dest: 'public/' }],
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "~@": fileURLToPath(new URL('./src', import.meta.url)),
+      '~@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
