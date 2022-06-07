f8a7443fa9d9971875f56d504a1860ed216bef40
Nate R <natemreinhardt@gmail.com>
2022-06-06 23:10:19 -0500

    notes and contact

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,16 +4,24 @@
   <div class="container welcome-container bg-gray-800 bg-opacity-10">
     <h3>Celestial Freight Train - A Celestial Empire Corporation</h3>
     <div>
-      Please make all contracts to
-      <a
-        href="https://evemaps.dotlan.net/corp/Celestial%20Freight%20Train"
-        target="_blank"
-        >Celestial Freight Train</a
-      >. Select your outbound (station we are picking up from) and your inbound
-      (station we are shipping to) from the available stations. Copy and paste
-      the items you want delievered into the "Package Details" text-area below.
-      Click "Run Qoute" to get a delivery quote. From there you can look at the
-      mocked contract creation screens to streamline the ingame process.
+      <br>
+      <p>
+        Please make all contracts to
+        <a href="https://evemaps.dotlan.net/corp/Celestial%20Freight%20Train" target="_blank">Celestial Freight
+          Train</a>.
+        Select your outbound (station we are picking up from) and your inbound
+        (station we are shipping to) from the available stations. Copy and paste
+        the items you want delievered into the "Package Details" text-area below.
+        Click "Run Qoute" to get a delivery quote. From there you can look at the
+        mocked contract creation screens to streamline the ingame process.
+      </p>
+      <br>
+      <p> If you have any questions - please reach out to Nate Armstrong (or Scott Armstrong) in game. </p>
+      <br>
+      <p>
+        NOTE: To best serve you - please make your you DO NOT have containers with or without items in them in your contract. Repackage any containers to be safe. Keep
+        your courier contracts a flat as possibles</p>
+
     </div>
   </div>
 </template>
@@ -22,6 +30,7 @@
 .welcome-container {
   padding: 2em;
 }
+
 h3 {
   font-size: 1.2rem;
 }
