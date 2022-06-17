6035688ace0e6b187378ca5203e09b3f5b9a5288
Nate R <natemreinhardt@gmail.com>
2022-06-17 09:41:16 -0500

    formatting

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+import QuoteLineItem from '@/components/quote/QuoteLineItem.vue';
+import { storeToRefs } from 'pinia';
+import { estimationStore } from '@/stores/estimation';
+
+const {
+  maxVolume,
+  maxCollateral,
+} = storeToRefs(estimationStore());
+</script>
 
 <template>
   <div class="container welcome-container bg-gray-800 bg-opacity-10">
@@ -19,8 +28,17 @@
       <p> If you have any questions - please reach out to Nate Armstrong (or Scott Armstrong) in game. </p>
       <br>
       <p>
-        NOTE: To best serve you - please make your you DO NOT have containers with or without items in them in your contract. Repackage any containers to be safe. Keep
-        your courier contracts a flat as possible</p>
+        <i><b>NOTE: </b></i> To best serve you - please follow these rules. Failure to follow these rules will result in the team unable to run your
+        contracts. If a single contract does not adhere to these rules, please try to break the contract into many.
+      <ol class="bullets">
+        <li>Do not have containers with or without items in them in your
+          contract. Repackage any containers to be safe. Keep
+          your courier contracts a flat as possible</li>
+        <li>Keep each contract under the Max Collateral of {{maxCollateral.toLocaleString()}} isk</li>
+        <li>Keep each contract under the Max Volume of {{maxVolume.toLocaleString()}} m³</li>
+        <li>Put the Janice Code as the description of your contract</li>
+      </ol>
+      </p>
 
     </div>
   </div>
@@ -34,4 +52,9 @@
 h3 {
   font-size: 1.2rem;
 }
+.bullets {
+  padding-left:2em;
+  list-style-type: circle;
+}
+
 </style>
